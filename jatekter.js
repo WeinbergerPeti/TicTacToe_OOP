c2b6432c39c509896e6168542527211b3e5efe8a
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-09-30 11:31:25 +0200

    Órán javítva

--- jatekter.js
+++ jatekter.js
@@ -1,14 +1,31 @@
 import Mezo from "./mezo.js";
 class Jatekter {
-
+    #mezoElemTomb=[];
     constructor() {
 
-        let mezoElem = $("article");
+        let szuloElem = $("article");
+        console.log(szuloElem);
         for (let index = 0; index < 9; index++) {
-            const elem = new Mezo(true, mezoElem);
-            mezoElem.push(elem);
+            const elem = new Mezo(true, szuloElem);
+            console.log(elem);
+            this.#mezoElemTomb.push(elem);
         }
+        console.log(this.#mezoElemTomb);
+    }
 
+    kattintas() {
+        if (this.#allapot) {
+            db++;
+            console.log(db);
+            if (db % 2 === 0) {
+                console.log("X");
+                this.kiir("X");
+            }
+            else if (db % 2 === 1) {
+                console.log("O");
+                this.kiir("O");
+            }
+        }
     }
 }
 
--- mezo.js
+++ mezo.js
@@ -10,7 +10,7 @@ class Mezo {
         elem.append(`<div class="mezo"><p class="elem"></p></div>`);
 
         this.mezoElem = elem.children("div:last-child");
-        console.log(this.mezoElem);
+        // console.log(this.mezoElem);
 
         // var db=0;
         this.mezoElem.on("click", () => {
@@ -24,20 +24,8 @@ class Mezo {
         this.#allapot = false;
     }
 
-    kattintas() {
-        if (this.#allapot) {
-            db++;
-            console.log(db);
-            if (db % 2 === 0) {
-                console.log("X");
-                this.kiir("X");
-            }
-            else if (db % 2 === 1) {
-                console.log("O");
-                this.kiir("O");
-            }
-        }
-    }
+
+    
 
     kiir(betu) {
         this.mezoElem.text(betu);
